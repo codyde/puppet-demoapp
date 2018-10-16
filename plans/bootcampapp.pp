ff56981936bd0e4fa6772bd5d600587f8c9f87b6
Codyde <codydearkland@gmail.com>
2018-10-15 18:11:14 -0700

    Update bootcampapp.pp

--- plans/bootcampapp.pp
+++ plans/bootcampapp.pp
@@ -1,70 +1,75 @@
 
   Package {'nginx':
          ensure => present,
-       }
+     }
 
-   exec { 'git clone':
+  exec { 'git clone':
      command => '/usr/bin/git clone -b standalone-p https://github.com/codyde/cmbu-bootcamp-app /tmp/cmbu-bootcamp-app'
-   }
+     }
 
 
-   file { "/tmp/cmbu-bootcamp-app/frontend-tier/src/app/app.component.html":
+  file { "/tmp/cmbu-bootcamp-app/frontend-tier/src/app/app.component.html":
     ensure  => file,
     force   => true,
     content => epp('/root/puppet-demoapp/modules/demoapp/templates/app.component.html.epp'),
     require => Exec['git clone']
     }
 
-   file { "/etc/nginx/conf.d/default.conf":
+  file { "/etc/nginx/conf.d/default.conf":
      ensure => file,
      force  => true, 
      source => '/tmp/cmbu-bootcamp-app/frontend-tier/nginx/default.conf',
      require => Exec['git clone']
      }
 
-   exec { 'remove default nginx':
+  exec { 'remove default nginx':
      command => '/bin/rm -rf /etc/nginx/sites-available/default',
      require => File['/etc/nginx/conf.d/default.conf']
      }
 
-    exec { 'install repo':
+  exec { 'install repo':
       command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -',
       require => File['/tmp/cmbu-bootcamp-app/frontend-tier/src/app/app.component.html']
       }
 
-    exec { 'install node':
+  exec { 'install node':
       command => '/usr/bin/apt install nodejs',
       require => Exec['install repo']
       }
 
-    exec { '@angular/cli':
-       command   => '/usr/bin/npm install -g @angular/cli',
-       require => Exec['install node']
+  exec { '@angular/cli':
+     command   => '/usr/bin/npm install -g @angular/cli',
+     require => Exec['install node']
        }
 
-   exec { 'npm install --unsafe-perm':
-	command => '/usr/bin/npm install',
-	cwd	=> '/tmp/cmbu-bootcamp-app/frontend-tier',
-	creates	=> '/tmp/cmbu-bootcamp-app/frontend-tier/node_modules',
-	require	=> Exec['@angular/cli']
-	}
+  exec { 'npm install':
+     command => '/usr/bin/npm i --save',
+     cwd     => '/tmp/cmbu-bootcamp-app/frontend-tier',
+     creates => '/tmp/cmbu-bootcamp-app/frontend-tier/node_modules',
+     require => Exec['@angular/cli']
+        }
 
-   exec { 'ng build --prod':
+  exec { 'ng build --prod':
      command => '/usr/bin/ng build --prod',
      cwd     => '/tmp/cmbu-bootcamp-app/frontend-tier',
-     creates => '/tmp/cmbu-bootcamp-app/frontend-tier/dist',
-     require => Exec['npm install --unsafe-perm']
+     creates => '/tmp/cmbu-bootcamp-app/frontend-tier/dist/cmbu-bootcamp-app',
+     require => Exec['npm install']
      }
 
-   file { '/usr/share/nginx/html':
+  file { '/usr/share/nginx/html':
         ensure => 'directory',
         source => '/tmp/cmbu-bootcamp-app/frontend-tier/dist/cmbu-bootcamp-app',
         recurse => true,
         force => true,
-	require => Exec['ng build --prod']
+        require => Exec['ng build --prod']
         }
 
-   exec { 'restart nginx':
+  exec { 'remove old nginx confs':
+        command => '/bin/rm -rf /etc/nginx/sites-available/* && /bin/rm -rf /etc/nginx/sites-enabled/*',
+        require => File['/usr/share/nginx/html']
+        }
+
+  exec { 'restart nginx':
        command => '/bin/systemctl restart nginx',
-       require => File['/usr/share/nginx/html']
-        } 
+       require => Exec['remove old nginx confs']
+        }
